827029468ab29a3dbe36c8467f670a389ea9e68e
Christian Caliendo <sepulchristian@gmail.com>
2020-06-23 22:09:14 +0200

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/udemy/ci-5-days/todo_app/';
+$config['base_url'] = 'ci3_todoapp/';
 
 $config['assets'] = array(
   'img' => $config['base_url'] . 'assets/img',
